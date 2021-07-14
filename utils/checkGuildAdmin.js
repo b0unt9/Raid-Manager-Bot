add99caf48d7817befef9155e4523041b0e8266b
SH <40918958+gitksh@users.noreply.github.com>
2021-07-15 08:22:47 +0900

    Update checkGuildAdmin.js
    Check guild raid admin role
    Check guild raid admin member

--- utils/checkGuildAdmin.js
+++ utils/checkGuildAdmin.js
@@ -1,5 +1,24 @@
+const guildSchema = require("../database/guildSchema");
+
 module.exports = async (message) => {
     try {
+        let adminData = await guildSchema.findOne({guildId: message.guild.id});
+        if (adminData && adminData.adminRole) {
+            await adminData.adminRole.forEach((roleId) => {
+                if (message.member.roles.cache.has(roleId)){
+                    return Boolean(true);
+                }
+            });
+        }
+
+        if (adminData && adminData.adminMember) {
+            await adminData.adminMember.forEach((memberId) => {
+                if (message.member.id === memberId) {
+                    return Boolean(true);
+                }
+            });
+        }
+
         if (await message.member.hasPermission('ADMINISTRATOR')) {
             return Boolean(true);
         }
