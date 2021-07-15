3714743055e3b10868f35d16725eacad6ea393bf
KSH <40918958+gitksh@users.noreply.github.com>
2021-07-16 08:16:40 +0900

    Rewrite the raid create commands
    Full code rewrite using synchronous processing

---
+++ commands/createDarkHorse.js
@@ -0,0 +1,24 @@
+const timeCheck = require("../utils/timeCheck");
+const createRaid = require("../utils/createRaid");
+const getEmbed = require("../utils/getEmbed");
+const setMsgDB = require("../utils/setMsgDB");
+const errorHandle = require("../utils/errorHandle");
+
+exports.run = async (client, message, args) => {
+    try {
+        let date = await timeCheck(args);
+        let raidData = await createRaid(message.guild.id, 3, message.member.user.id, date);
+        let embed = await getEmbed(client, raidData, 1, raidData.master);
+        let msg = await message.channel.send(embed);
+        await setMsgDB(msg.guild.id, msg.channel.id, msg.id, raidData.raidId);
+        return msg.react("🤚");
+    } catch (err) {
+        let errMsg = await errorHandle(err);
+        return message.channel.send(errMsg);
+    }
+};
+
+exports.config = {
+    name: "칠흑&철마 레이드 생성",
+    commands: ["흑마", "di"]
+};
