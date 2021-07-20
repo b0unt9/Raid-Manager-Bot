c1d7399213b3f5b838170b2fd0506c74bcc438fc
KSH <40918958+gitksh@users.noreply.github.com>
2021-07-20 16:50:39 +0900

    Fixed an error where the bot name did not appear in the invite command Embed

--- commands/msgInvite.js
+++ commands/msgInvite.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
     try {
         let embed = new Discord.MessageEmbed();
         embed.setTitle('디비전 레이드 초대 주소')
-            .setAuthor(`${client.user.name}`, `${client.user.displayAvatarURL()}`, '')
+            .setAuthor(`${client.user.username}`, `${client.user.displayAvatarURL()}`, '')
             .addField('봇 초대', 'https://top.gg/bot/781757459307823144')
             .addField('Github Repo', 'https://github.com/sidetoy/The-Division-2-Raid-Manager-Bot')
             .setFooter('개발 : SideToy / 운영 : FlareBrick / 문의 : dbots@flarebrick.com');
